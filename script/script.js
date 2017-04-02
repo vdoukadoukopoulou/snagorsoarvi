5fde2b24c1010e11c394dcabb02650ab9e50475a
Stef Kors <stef.kors@gmail.com>
2017-04-02 21:58:59 +0200

    added lots

--- script/script.js
+++ script/script.js
@@ -6,25 +6,20 @@ $(document).ready(function() {
 
 	var numOfPanels = $('.panel').length;
 	var currentPanel = 0;
-	var panelHeight = 0;
-
-	//set height of page same as height of all panels combined
-	$('.panel').each(function() {
-		panelHeight = panelHeight + $(this).height();
-	});
-	if (panelHeight > 25878) {
-		$('body').height(panelHeight);
-	} else {
-		$('body').height(25878);
-	}
 
 	//set first panel to absolute
 	$('.panel').eq(currentPanel).css({position: 'absolute'});
+
 	// on scroll check if the current active panel is visible if not make next panel active
-	$(window).scroll(function() {
-		if (checkVisible($('.panel').eq(currentPanel)) != true) {
-			$('.panel').eq(currentPanel).next().css({position: 'absolute', top: $(window).scrollTop()});
-			currentPanel++;
+	$(window).bind('mousewheel', function(event) {
+		if (event.originalEvent.wheelDelta >= 0) {
+			console.log('Scroll up');
+		} else {
+			console.log('Scroll down');
+			if (checkVisible($('.panel').eq(currentPanel)) != true) {
+				$('.panel').eq(currentPanel).next().css({position: 'absolute', top: $(window).scrollTop()});
+				currentPanel++;
+			}
 		}
 	});
 
@@ -35,19 +30,11 @@ $(document).ready(function() {
 			st = $(window).scrollTop(), // Scroll Top
 			y = $(elm).offset().top,
 			elementHeight = $(elm).height();
-
 		if (eval == "visible")
 			return ((y < (vpH + st)) && (y > (st - elementHeight)));
 		if (eval == "above")
 			return ((y < (vpH + st)));
 		};
-
-	//this part does the randomizing panels
-	var random = $('.random');
-
-	for (var i = 0; i < random.length; i++) {
-		console.log(random[i]);
-	};
 
 	//click to make panel to foreground
 
@@ -58,4 +45,19 @@ $(document).ready(function() {
 	$('.close').click(function() {
 		$('.references').removeClass('front');
 	});
+});
+
+$(window).load(function() {
+	var panelHeight = 0;
+
+	//set height of page same as height of all panels combined
+	$('.panel').each(function() {
+		panelHeight = panelHeight + $(this).height();
+	});
+	console.log(panelHeight);
+	if (panelHeight > 25878) {
+		$('body').height(panelHeight);
+	} else {
+		$('body').height(25878);
+	}
 });
